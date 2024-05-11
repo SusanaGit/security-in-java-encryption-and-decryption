483543a8c2c54eb274e49c04b9c29fff33aa9fc3
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-05-11 16:40:10 +0200

    5. Inicializo generadorClaveSecreta con el tamaño de 128 para que cree claves secretas de este tamaño

--- RSA/src/main/java/AES.java
+++ RSA/src/main/java/AES.java
@@ -1,12 +1,20 @@
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import java.security.NoSuchAlgorithmException;
 
 public class AES {
 
     private SecretKey claveSecreta;
 
-    public void init() {
+    public void init() throws NoSuchAlgorithmException {
         KeyGenerator generadorClaveSecreta =  KeyGenerator.getInstance("AES");
+
+        /*
+        El tamaño de las claves secretas generadas por el objeto de KeyGenerator
+        puede ser de 128, 192 o 256
+         */
+        int KEY_SIZE = 128;
+        generadorClaveSecreta.init(KEY_SIZE);
     }
 
 }
