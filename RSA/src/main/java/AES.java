5caa60f796fa419d9082d7fe82a1604cb82b720f
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-05-11 16:34:31 +0200

    4. Creo método init() que generará objeto de tipo KeyGenerator

--- RSA/src/main/java/AES.java
+++ RSA/src/main/java/AES.java
@@ -1,10 +1,12 @@
+import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 
 public class AES {
 
     private SecretKey claveSecreta;
 
-
-
+    public void init() {
+        KeyGenerator generadorClaveSecreta =  KeyGenerator.getInstance("AES");
+    }
 
 }
